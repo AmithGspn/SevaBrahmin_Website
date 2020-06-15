40c194bcdc0345c848d64baf323d5ebb0f6f23c0
amithgspn_23 <amith@opennets.com>
2020-06-15 21:09:14 +0530

    Login error classes defined

--- server/routes/loginManager.js
+++ server/routes/loginManager.js
@@ -3,19 +3,31 @@ let router = express.Router();
 let jwt = require('jsonwebtoken');
 let registerationModel = require('../db/models/registeration');
 
-router.post('/', (req, res) => {
+router.post('/', (req, res,next) => {
     data = req.body;
     registerationModel.findOne({ email: data.email}, (error, user)  => {
         if(error) {
             console.log(error)
         } else {
             if (!user) {
-                res.status(401).send('Invalid email')
+                try {
+                    throw new Error('Invalid Email');
+                } catch (er) {
+                    return next(er)
+                }
             } else {
                 if (user.password !== data.password) {
-                    res.status(401).send('Invalid Passowrd') 
+                    try {
+                        throw new Error('Invalid Password');
+                    } catch (er) {
+                        return next(er)
+                    }
                 } else if (user.approved === false) {
-                    res.status(401).send('Account has not been activated yet')
+                    try {
+                        throw new Error('Account has not been activated yet');
+                    } catch (er) {
+                        return next(er)
+                    }
                 } else {
                     let payload = { subject: user._id };
                     let token = jwt.sign(payload, 'secretKey')
